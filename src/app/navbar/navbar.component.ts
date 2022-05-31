3e5decbb49300b47c6b4fa20a641ad938392a7c8
Vinicius <vinicius.b.silva3@gmail.com>
2022-05-31 16:05:49 -0300

    Commit inicial

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  @ViewChild('navContainer') navContainer!: ElementRef;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onNavbarToggle() {
+    this.navContainer.nativeElement.classList.toggle('collapsed');
+  }
+}
