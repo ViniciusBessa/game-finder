b293b0d161f9d0bacc549a402ff53696c0cf7d7c
Vinicius <vinicius.b.silva3@gmail.com>
2022-06-12 23:12:13 -0300

    fix: cover do jogo na página game-page ficava esticado se a descrição fosse grande, e texto do game-item não ficava centralizado no mobile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  igdbProxyUrl: 'https://igdb-proxy-api.herokuapp.com/api/v1',
+  igdbImageUrl: 'https://images.igdb.com/igdb/image/upload',
 };
