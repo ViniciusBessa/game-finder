085197bc5042e8c27b60ef0ee823ed322a07a39f
Vinícius Bessa <vinicius.b.silva3@gmail.com>
2023-01-03 14:19:29 -0300

    feat: url da api foi atualizado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  igdbProxyUrl: 'https://igdb-proxy-api.herokuapp.com/api/v1',
+  igdbProxyUrl: 'https://igdb-proxy-api.onrender.com/api/v1',
   igdbImageUrl: 'https://images.igdb.com/igdb/image/upload',
 };
